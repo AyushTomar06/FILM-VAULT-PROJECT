cd3157526152770fd013d6b2545e845ca2d3bb35
AyushTomar06 <ayushgurjar110@gmail.com>
2024-12-24 11:52:50 +0530

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   
-base: "/FILMVAULT-PROJECT/",
+base: "/FILM-VAULT-PROJECT/",
   plugins: [react()],
 })
