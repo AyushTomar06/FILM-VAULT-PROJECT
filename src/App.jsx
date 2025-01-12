c454cec1f2c3d461dff9081eff2e4ef3e4e95d66
AyushTomar06 <ayushgurjar110@gmail.com>
2025-01-12 16:58:48 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -47,14 +47,14 @@ export default function App() {
 
         <Routes>
           <Route
-            path="/FILM-VAULT-PROJECT/"
+            path="/FILM-VAULT-PROJECT/FILM-VAULT-PROJECT/"
             element={
               <>
                  <Banner /> <Movies removefromwatchlist={removewatchlist} addmovietowatchlist={addmovietowatchlist} watchlist={watchlist} />
               </>
             }
           />
-          <Route path="/FILM-VAULT-PROJECT/WatchList/" element={< Watchlist Watchlist={watchlist} removefromwatchlist={removewatchlist} />} />
+          <Route path="FILM-VAULT-PROJECT/FILM-VAULT-PROJECT/WatchList/" element={< Watchlist Watchlist={watchlist} removefromwatchlist={removewatchlist} />} />
         </Routes>
       </BrowserRouter>
     </>
